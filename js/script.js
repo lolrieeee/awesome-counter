4af237f707c04d7ecd389925957e454654b9d559
Larry Bui <lolrieeee>
2017-03-27 12:59:50 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+(function() {
+
+    'use strict';
+
+    var i = 0;
+    console.log(i)
+
+
+    $('.add-score').on('click', function(){
+        i = parseInt( $(this).parent('.player').children('.player-score').html() );
+        if( i < 15 ) {
+            i++;
+            if ( i > 0 ) {
+                $(this).parent('.player').children('.player-score').css('color', 'pink');
+            }
+
+            if ( i > 5 ) {
+                $(this).parent('.player').children('.player-score').css('color', 'orange');
+            }
+            if ( i > 10) {
+                $(this).parent('.player').children('.player-score').css('color', 'green');
+            }
+            $(this).parent('.player').children('.player-score').html(i);
+            console.log(i);
+        }
+    });
+
+
+
+    $('.minus-score').on('click', function(){
+        i = parseInt( $(this).parent('.player').children('.player-score').html() );
+        if ( i > 0 ) {
+            i--;
+            if ( i < 15 ) {
+                $(this).parent('.player').children('.player-score').css('color', 'green');
+            }
+
+            if ( i < 10 ) {
+                $(this).parent('.player').children('.player-score').css('color', 'orange');
+            }
+            if ( i < 5) {
+                $(this).parent('.player').children('.player-score').css('color', 'pink');
+            }
+            $(this).parent('.player').children('.player-score').html(i);
+            console.log(i);
+        }
+    });
+
+}());
