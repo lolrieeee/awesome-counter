64cf466008de09d35f02e07ff29fdfa4a5b8fe78
Larry Bui <lolrieeee>
2017-03-27 16:01:01 -0700

    sounds

--- js/script.js
+++ js/script.js
@@ -10,15 +10,22 @@
         i = parseInt( $(this).parent('.player').children('.player-score').html() );
         if( i < 15 ) {
             i++;
-            if ( i > 0 ) {
+            if ( i > 0 && i < 15 ) {
                 $(this).parent('.player').children('.player-score').css('color', 'violet');
+                document.getElementById('point-gain').play();
             }
 
-            if ( i > 5 ) {
+            if ( i > 5 && i < 15) {
                 $(this).parent('.player').children('.player-score').css('color', 'orange');
+                document.getElementById('point-gain').play();
             }
-            if ( i > 10) {
+            if ( i > 10 && i < 15 ) {
                 $(this).parent('.player').children('.player-score').css('color', 'green');
+                document.getElementById('point-gain').play();
+            }
+            if ( i == 15 ) {
+                $(this).parent('.player').children('.player-score').css('color', 'blue');
+                document.getElementById('winner').play();
             }
             $(this).parent('.player').children('.player-score').html(i);
             console.log(i);
@@ -31,19 +38,28 @@
         i = parseInt( $(this).parent('.player').children('.player-score').html() );
         if ( i > 0 ) {
             i--;
-            if ( i < 15 ) {
+            if ( i < 15 && i !== 0 ) {
                 $(this).parent('.player').children('.player-score').css('color', 'green');
+                document.getElementById('point-loss').play();
             }
 
-            if ( i < 10 ) {
+            if ( i < 10 && i !== 0 ) {
                 $(this).parent('.player').children('.player-score').css('color', 'orange');
+                document.getElementById('point-loss').play();
             }
-            if ( i < 5) {
+            if ( i < 5 && i !== 0 ) {
                 $(this).parent('.player').children('.player-score').css('color', 'violet');
+                document.getElementById('point-loss').play();
+            }
+
+            if ( i === 0 ) {
+                document.getElementById('zero').play();
             }
             $(this).parent('.player').children('.player-score').html(i);
             console.log(i);
+
         }
     });
+
 
 }());
